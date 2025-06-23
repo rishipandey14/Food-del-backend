6a3ad19de6b9507ae014b69d6fa86da7c4dcc711
Rishi <hopesalive1403@gmail.com>
2025-06-23 21:10:30 +0530

    Sale and Rating API

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import RestaurantList, RestaurantDetail
+from .views import RestaurantList, RestaurantDetail, SaleList, SaleDetail, RatingList, RatingDetail
 
 
 urlpatterns = [
-  path('restaurants/', RestaurantList.as_view()),
-  path('restaurants/<int:pk>/', RestaurantDetail.as_view()),
+  path('restaurants/', RestaurantList.as_view(), name='restaurant-list'),
+  path('restaurants/<int:pk>/', RestaurantDetail.as_view(), name='restaurant-detail'),
+  
+  path('sales/', SaleList.as_view(), name='sale-list'),
+  path('sales/<int:pk>', SaleDetail.as_view(), name='sale-detail'),
+  
+  path('ratings/', RatingList.as_view(), name='rating-list'),
+  path('ratings/<int:pk>', RatingDetail.as_view(), name='rating-detail'),
+  
 ]
--- base/views.py
+++ base/views.py
@@ -59,3 +59,116 @@ class RestaurantDetail(APIView):
         },status=status.HTTP_204_NO_CONTENT)
 
 
+class SaleList(APIView):
+    def get(self, request):
+        restaurant_id = request.query_params.get('restaurant', None)
+        if restaurant_id:
+            sales = Sale.objects.filter(restaurant__id = restaurant_id)
+        else :
+            sales = Sale.objects.all()
+            
+        serializer = SaleSerializer(sales, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = SaleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message": "Sale Record Created successfully",
+                "data": serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SaleDetail(APIView):
+    def get(self, request, pk):
+        try:
+            sale = Sale.objects.get(pk=pk)
+        except Sale.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = SaleSerializer(sale)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        try:
+            sale = Sale.objects.get(pk=pk)
+        except Sale.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = SaleSerializer(sale, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message" : "Sale updated successfully", 
+                "data" : serializer.data
+            })
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        try:
+            sale = Sale.objects.get(pk=pk)
+        except Sale.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        sale.delete()
+        return Response({
+            "message" : "Deleted Successfully"
+        },status=status.HTTP_204_NO_CONTENT)
+
+
+
+class RatingList(APIView):
+    def get(self, request):
+        user_id = request.query_params.get('user', None)
+        restaurant_id = request.query_params.get('restaurant', None)
+        
+        ratings = Rating.objects.all()
+        if user_id:
+            ratings = ratings.filter(user__id = user_id)
+        if restaurant_id:
+            ratings = ratings.filter(restaurant__id = restaurant_id)
+        serializer = RatingSerializer(ratings, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = RatingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message": "Rating Submitted successfully",
+                "data": serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RatingDetail(APIView):
+    def get(self, request, pk):
+        try:
+            rating = Rating.objects.get(pk=pk)
+        except Rating.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = RatingSerializer(rating)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        try:
+            sale = Rating.objects.get(pk=pk)
+        except Rating.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = RatingSerializer(sale, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message" : "Rating updated successfully", 
+                "data" : serializer.data
+            })
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        try:
+            rating = Rating.objects.get(pk=pk)
+        except Rating.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        rating.delete()
+        return Response({
+            "message" : "Deleted Successfully"
+        },status=status.HTTP_204_NO_CONTENT)
